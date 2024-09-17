76e4f6d34ff6f2bf1a9c56598b461d785bf1619d
B2005848 <namltgt2002@gmail.com>
2024-09-17 22:05:43 +0700

    upload

--- src/layouts/account.login.vue
+++ src/layouts/account.login.vue
@@ -1,3 +1,37 @@
 <template>
-  <h1>TRANG LOGIN ADMIN</h1>
+  <div class="back-ground">
+    <h1>TRANG LOGIN ADMIN</h1>
+  </div>
 </template>
+
+<style scoped>
+.back-ground {
+  width: 100vw;
+  height: 100vh;
+  background-image: url(background.svg);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+}
+
+h1 {
+  font-size: 24px; /* Kích thước chữ mặc định cho màn hình nhỏ */
+}
+
+/* Thiết bị có màn hình từ 768px trở lên */
+@media (min-width: 768px) {
+  h1 {
+    font-size: 36px; /* Tăng kích thước chữ cho màn hình lớn hơn */
+  }
+}
+
+/* Thiết bị có màn hình từ 1024px trở lên */
+@media (min-width: 1024px) {
+  h1 {
+    font-size: 48px; /* Kích thước lớn cho màn hình desktop */
+  }
+}
+</style>
+
+<script setup></script>
