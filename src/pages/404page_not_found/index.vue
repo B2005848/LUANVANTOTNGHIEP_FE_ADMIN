0544072ff5930aff4b3a8eea0a4283dd97a7f3be
B2005848 <namltgt2002@gmail.com>
2024-09-17 21:09:47 +0700

    update router page not found

---
+++ src/pages/404page_not_found/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="not-found">
+    <h1>404 - Page Not Found</h1>
+    <p>Sorry, the page you're looking for doesn't exist.</p>
+    <router-link to="/admin">Go back to Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
+
+<style scoped>
+.not-found {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
--- src/router/index.router.js
+++ src/router/index.router.js
@@ -1,15 +1,30 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Admin from "./admin.router";
-
-const routes = [...Admin];
-
+import { useAuthStore } from "@/stores/authenticate-login";
+import PageNotFound from "@/pages/404page_not_found/index.vue";
+const routes = [
+  ...Admin,
+  {
+    path: "/:pathMatch(.*)*",
+    name: "not-found",
+    component: PageNotFound,
+    meta: {
+      title: "404 - Page Not Found",
+    },
+  },
+];
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
 });
 
 router.beforeEach((to, from, next) => {
-  document.title = to.meta?.title ?? "Error 404 - PAGE NOT FOUND!!!";
-  next();
+  const authStore = useAuthStore();
+  document.title = to.meta?.title ?? "No title page!!!";
+  if (to.path.startsWith("/admin") && !authStore.isLogged) {
+    next({ name: "admin.login" });
+  } else {
+    next();
+  }
 });
 export default router;
