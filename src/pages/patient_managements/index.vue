cd3ff93ea5e2d8f75272547f0ff189e15c1a1b18
B2005848 <namltgt2002@gmail.com>
2024-09-20 01:15:54 +0700

    update

--- src/pages/patient_managements/index.vue
+++ src/pages/patient_managements/index.vue
@@ -1,190 +1,223 @@
-<style scoped>
-.card .d-flex {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-</style>
-
 <template>
   <div>
+    <!-- Quản lí nhân viên -->
     <div class="container-fluid mt-3">
-      <div class="card p-2">
-        <div class="d-flex">
+      <div class="wapper card p-3">
+        <div class="d-flex mt-3">
           <div class="flex-1">
-            <h3 class="card-title">Danh sách tài khoản bệnh nhân</h3>
+            <h3 class="">Danh sách tài khoản bệnh nhân</h3>
           </div>
-          <div class="flex-1">
-            <input
-              type="text"
-              id="filterName"
-              v-model="filterName"
-              @input="filterUsers"
-              class="form-control"
-              size="50"
-              placeholder="Nhập mã bệnh nhân muốn tìm......"
-            />
-          </div>
-          <div class="flex-1">
-            <router-link
-              :to="{
-                name: 'admin.add.patient',
-              }"
-              ><span style="align-items: center; margin-right: 5px"
-                >Thêm bệnh nhân mới</span
-              >
-              <font-awesome-icon
-                icon="fa-solid fa-plus-minus"
-                bounce
-                style="color: #74c0fc; font-size: 30px"
-              />
-            </router-link>
-          </div>
-        </div>
-        <div class="card-body">
-          <div class="table-responsive">
-            <table class="table table table-bordered table-hover table-striped">
-              <thead>
-                <tr class="text-center">
-                  <th>No.</th>
-                  <th @click="sortTable('Username')">Tài khoản</th>
-                  <th>Họ và tên</th>
-                  <th @click="sortTable('Email')">Email</th>
-                  <th @click="sortTable('Phone')">Số điện thoại</th>
-                  <th @click="sortTable('StatusID')">Trạng thái</th>
-                  <th @click="sortTable('RegistrationDate')">Ngày đăng ký</th>
-                  <th>Tools</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr
-                  class="text-center"
-                  v-for="(record, index) in users"
-                  :key="index"
+          <div class="d-flex">
+            <form class="tw-max-w-md tw-mx-auto">
+              <div class="tw-relative">
+                <div
+                  class="tw-absolute tw-inset-y-0 tw-start-0 tw-flex tw-items-center tw-ps-3 tw-pointer-events-none"
                 >
-                  <td>{{ (currentPage - 1) * itemsPerPage + index + 1 }}</td>
-                  <td>{{ record.username }}</td>
-                  <td>{{ record.name }}</td>
-                  <td v-if="record.email">{{ record.email }}</td>
-                  <td v-if="!record.email" style="color: coral">
-                    Chưa cập nhật
-                  </td>
-                  <td v-if="record.phone_number">
-                    {{ record.phone_number }}
-                  </td>
-                  <td v-if="!record.phone_number" style="color: coral">
-                    Chưa cập nhật
-                  </td>
-                  <td v-if="record.statusAccount == 1" class="text-success">
-                    Đang hoạt động
-                  </td>
-                  <td v-if="record.statusAccount == 2" class="text-warning">
-                    Tạm khóa
-                  </td>
-                  <td v-if="record.statusAccount == 3" class="text-danger">
-                    Ngừng hoạt động
-                  </td>
-                  <td>{{ formatDatetime(record.registration_date) }}</td>
-                  <td>
-                    <router-link
-                      :to="{
-                        name: 'admin.patient_details',
-                        params: { username: record.username },
-                      }"
-                    >
-                      Chi tiết</router-link
-                    >
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+                  <svg
+                    class="tw-w-4 tw-h-4 tw-text-gray-500 dark:tw-text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                    />
+                  </svg>
+                </div>
+                <input
+                  type="search"
+                  v-model="searchData"
+                  id="default-search"
+                  size="50"
+                  class="tw-block tw-w-full tw-p-4 tw-ps-10 tw-text-sm tw-text-gray-900 tw-border tw-border-gray-300 tw-rounded-lg tw-bg-gray-50 focus:tw-ring-blue-500 focus:tw-border-blue-500 dark:tw-bg-gray-700 dark:tw-border-gray-600 dark:tw-placeholder-gray-400 dark:tw-text-white dark:focus:tw-ring-blue-500 dark:focus:tw-border-blue-500"
+                  placeholder="Nhập thông tin tài khoản cần tìm..."
+                  required
+                />
+                <button
+                  @click="handleSearch"
+                  type="button"
+                  class="tw-text-white tw-absolute tw-end-2.5 tw-bottom-2.5 tw-bg-blue-500 hover:tw-bg-blue-800 focus:tw-ring-4 focus:tw-outline-none focus:tw-ring-blue-300 tw-font-normal tw-rounded-lg tw-text-sm tw-px-4 tw-py-2 dark:tw-bg-blue-600 dark:hover:tw-bg-blue-700 dark:focus:tw-ring-blue-800"
+                >
+                  Tìm kiếm
+                </button>
+              </div>
+            </form>
           </div>
         </div>
-      </div>
-      <nav class="mt-3" aria-label="Page navigation">
-        <!-- case 1 use pagination vue-->
-        <paginate
-          v-model="currentPage"
-          :page-count="totalPages"
-          :page-range="3"
-          :margin-pages="1"
-          :click-handler="changePage"
-          :prev-text="'Prev'"
-          :next-text="'Next'"
-          :container-class="'pagination'"
-          :page-class="'page-item'"
+
+        <!-- --TW CSS -->
+        <!-- list emp -->
+        <div
+          class="mt-5 tw-relative tw-overflow-x-auto tw-shadow-md tw-sm:rounded-lg"
         >
-        </paginate>
-      </nav>
+          <table
+            class="tw-w-full tw-text-sm tw-text-left tw-rtl:text-right tw-text-gray-800 tw-dark:text-gray-400"
+          >
+            <thead
+              class="tw-text-xs tw-text-gray-700 tw-uppercase tw-bg-gray-200 tw-dark:bg-gray-700 tw-dark:text-gray-400"
+            >
+              <tr class="tw-text-center">
+                <th scope="col" class="tw-px-6 tw-py-3">STT</th>
+                <th scope="col" class="tw-px-6 tw-py-3">Mã bệnh nhân</th>
+                <th scope="col" class="tw-px-6 tw-py-3">Họ và tên</th>
+                <th scope="col" class="tw-px-6 tw-py-3">Ngày sinh</th>
+                <th scope="col" class="tw-px-6 tw-py-3">Email</th>
+                <th scope="col" class="tw-px-6 tw-py-3">Số CCCD/CMND</th>
+                <th scope="col" class="tw-px-6 tw-py-3">Trạng thái</th>
+                <th scope="col" class="tw-px-6 tw-py-3">Ngày tạo</th>
+                <th scope="col" class="tw-px-6 tw-py-3">Ngày chỉnh sửa</th>
+                <th scope="col" class="tw-px-6 tw-py-3">Hồ sơ bệnh án</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="(pa, index) in listPatientsData"
+                :key="index"
+                class="tw-bg-white tw-text-center tw-border-b tw-dark:bg-gray-800 tw-dark:border-gray-700 tw-hover:bg-gray-50 tw-dark:hover:bg-gray-600"
+              >
+                <!-- STT -->
+                <th
+                  scope="row"
+                  class="tw-px-6 tw-py-4 tw-font-medium tw-text-gray-900 tw-whitespace-nowrap tw-dark:text-white"
+                >
+                  {{ (currentPage - 1) * itemsPerPageData + index + 1 }}
+                </th>
+                <td>{{ pa.patient_id }}</td>
+                <td>{{ pa.first_name }} {{ pa.last_name }}</td>
+                <td class="px-6 py-4">{{ formatBirthDay(pa.birthday) }}</td>
+                <td class="px-6 py-4">{{ pa.email }}</td>
+                <td class="px-6 py-4">
+                  {{ pa.citizen_id || "Chưa cập nhật" }}
+                </td>
+                <td class="px-6 py-4">
+                  <span class="text-success" v-if="pa.status === '1'"
+                    >Đang hoạt động</span
+                  >
+                  <span class="text-danger" v-if="pa.status === '0'"
+                    >Ngừng hoạt động</span
+                  >
+                  <span class="text-warning" v-if="pa.status === '2'"
+                    >Tạm khóa</span
+                  >
+                </td>
+                <td class="px-6 py-4">{{ formatDateTime(pa.created_at) }}</td>
+                <td class="px-6 py-4">{{ formatDateTime(pa.updated_at) }}</td>
+
+                <td class="px-6 py-4">
+                  <router-link
+                    :to="{
+                      name: 'admin.emp_details',
+                      params: { id: pa.patient_id },
+                    }"
+                  >
+                    Chi tiết
+                  </router-link>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <!-- Pagination -->
+        <PaginationComponent
+          :pageCount="totalPagesData"
+          :currentPage="currentPage"
+          @page-change="fetchDataByPage"
+        />
+      </div>
     </div>
+
     <router-view></router-view>
   </div>
 </template>
 
+<!----------------------------------------------------------SCRIP SETUP----------------------------------------------->
 <script setup>
-import { onMounted, ref, watch } from "vue";
-import Paginate from "vuejs-paginate-next";
-import moment from "moment";
+import { onMounted, ref } from "vue";
+import {
+  handleGetListPatients,
+  listPatientsData,
+  errorMessage,
+  totalPagesData,
+  itemsPerPageData,
+} from "@/services/patient_managements/handleGetListPatient";
+import { searchPatients } from "@/services/patient_managements/handleSearchPatients";
+import PaginationComponent from "@/components/Pagination.vue";
+import formatDate from "@/helper/format-datetime";
+const formatDateTime = formatDate.formatDateTime;
+const formatBirthDay = formatDate.formatDateBirth;
+const currentPage = ref(1);
 
-// Hàm định dạng thời gian theo UTC mà không cần chuyển đổi múi giờ
-const formatDatetime = (datetime) => {
-  const formattedDatetime = moment.utc(datetime).format("DD/MM/YYYY (HH:mm)");
-  console.log("Formatted datetime (UTC):", formattedDatetime); // Kiểm tra giá trị datetime sau khi định dạng
-  return formattedDatetime;
-};
-const users = ref([]);
-const totalPages = ref(0);
-let currentPage = ref(1);
-const itemsPerPage = 5;
-const changePage = (page) => {
+const fetchDataByPage = async (page) => {
   currentPage.value = page;
-  getUsers(page);
+  await handleGetListPatients(page);
 };
-let originalUsers = [];
 
-// GET_DATA
-const getUsers = async (page) => {
-  try {
-    const response = await window.axios.get(
-      `http://localhost:3000/api/handle/patient/getlistpatients/?page=${page}`
-    );
-    totalPages.value = response.data.totalPages;
-
-    if (Array.isArray(response.data.patients)) {
-      users.value = response.data.patients;
-      originalUsers.value = response.data.patients;
+// Xử lý tìm kiếm tài khoản nhân viên
+const searchData = ref("");
+const handleSearch = async () => {
+  if (searchData.value) {
+    const resultDataSearch = await searchPatients(searchData.value);
+    if (resultDataSearch && resultDataSearch.data.length > 0) {
+      listPatientsData.value = resultDataSearch.data;
     } else {
-      originalUsers.value = [];
+      errorMessage.value = "Không tìm thấy tài khoản nào phù hợp.";
     }
-
-    console.log(originalUsers.value);
-  } catch (error) {
-    console.error(error);
+  } else {
+    await fetchDataByPage(1);
   }
 };
 
-// FILTER
-const filterName = ref("");
-const filterUsers = async () => {
-  if (!originalUsers.length) {
-    originalUsers = [...users.value];
-    await getUsers(1);
-  }
-  await getUsers(currentPage.value);
-  const filteredUsers = originalUsers.filter((user) => {
-    const nameCondition = user.username
-      .toLowerCase()
-      .includes(filterName.value.toLowerCase());
-
-    return nameCondition;
-  });
-
-  users.value = filteredUsers;
-};
-
-watch(filterName, filterUsers);
-
-onMounted(() => {
-  getUsers(currentPage.value);
+onMounted(async () => {
+  await fetchDataByPage(1);
 });
 </script>
+
+<!------------------------------------------------------CSS SCOPED------------------------------------------>
+<style scoped>
+.wapper .d-flex {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.btn-search {
+  background-color: #007bff;
+  border-radius: 14px;
+}
+
+.d-flex div:nth-child(2) button {
+  color: #ffffff;
+  font-size: 25px;
+  text-align: center;
+  align-items: center;
+  border: none;
+}
+
+.pagination {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.page-item {
+  margin: 0 5px;
+}
+
+.page-link {
+  padding: 5px 10px;
+  border: 1px solid #ccc;
+  background-color: #f8f9fa;
+  cursor: pointer;
+}
+
+.page-link:hover {
+  background-color: #007bff;
+  color: white;
+}
+</style>
--- src/services/patient_managements/handleGetListPatient.js
+++ src/services/patient_managements/handleGetListPatient.js
@@ -1,38 +1,32 @@
 import { ref } from "vue";
 
-export const handleGetListPatients = () => {
-  const errorMessage = ref("");
-  const listPatients = ref([]);
-  const totalPagesData = ref(0);
-  const itemsPerPageData = ref(10);
-  const getData = async (page = 1) => {
-    try {
-      const response = await window.axios.get(
-        `http://localhost:3000/api/departments/getList/?page=${page}`
-      );
-      if (response.status === 200) {
-        const { totalPages, listDepartments, itemsPerPage } = response.data;
-        totalPagesData.value = totalPages;
-        listDepartmentsData.value = listDepartments;
-        itemsPerPageData.value = itemsPerPage;
-        console.log(
-          "Get list departments success",
-          totalPages,
-          listDepartments
-        );
-      }
-    } catch (error) {
-      errorMessage.value =
-        error.response?.data?.message || "Get list departments FAILS";
-      console.log(error.response.data.message);
+const errorMessage = ref("");
+const listPatientsData = ref([]);
+const totalPagesData = ref(0);
+const itemsPerPageData = ref(10);
+const handleGetListPatients = async (page = 1) => {
+  try {
+    const response = await window.axios.get(
+      `http://localhost:3000/api/handle/patient/getlistaccountpatients/?page=${page}`
+    );
+    if (response.status === 200) {
+      const { totalPages, patients, itemsPerPage } = response.data;
+      totalPagesData.value = totalPages;
+      listPatientsData.value = patients;
+      itemsPerPageData.value = itemsPerPage;
+      console.log("Get list patients success", totalPages, listDepartments);
     }
-  };
+  } catch (error) {
+    errorMessage.value =
+      error.response?.data?.message || "Get list patients FAILS";
+    console.log(error.response.data.message);
+  }
+};
 
-  return {
-    getData,
-    errorMessage,
-    totalPagesData,
-    listDepartmentsData,
-    itemsPerPageData,
-  };
+export {
+  handleGetListPatients,
+  errorMessage,
+  totalPagesData,
+  listPatientsData,
+  itemsPerPageData,
 };
