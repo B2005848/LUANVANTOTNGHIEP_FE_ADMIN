f9a69b4257b83786906b98a5c8e341256d0fdb98
B2005848 <namltgt2002@gmail.com>
2024-11-28 18:27:03 +0700

    update

---
+++ src/pages/booking_managements/booking_old.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-6">
+        <h4 class="text-center">VUI LÒNG NHẬP MÃ SỐ BỆNH NHÂN</h4>
+
+        <input
+          type="text"
+          class="form-control mb-3"
+          placeholder="vd: N18-XXXXX"
+        />
+        <button
+          style="background-color: #307be7; color: aliceblue; font-weight: bold"
+          class="form-control"
+        >
+          XÁC NHẬN
+        </button>
+      </div>
+
+      <!-- <----------------------------------COL-2--------------------------------------------- -->
+      <div class="col-md-6">
+        <p>Xem cách tìm mã số bệnh nhân</p>
+        <img
+          src="https://umc-v1.medpro.vn/asset/images/logged-step/bienlai.png"
+          alt=""
+          width="500px"
+          srcset=""
+        />
+      </div>
+    </div>
+  </div>
+</template>
